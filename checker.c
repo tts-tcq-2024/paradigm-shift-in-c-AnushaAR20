e328fdf5c9be240e985d1315baa5686e762691d6
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-06-28 16:24:48 +0530

    checker5.c
    Reduced complexity

--- checker.c
+++ checker.c
@@ -1,35 +1,46 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 #include <assert.h>
 
-int check(float min, float max, float val, char* parameter) 
+typedef struct {
+  float min;
+  float max;
+  float value;
+  const char* parameter;
+} Parameter;
+
+int isInRange(Parameter param) 
 {
-  if (val < min || val > max) 
+  if (param.value >= param.min && param.value <= param.max) 
   {
-    printf("%s is Out of range \n", parameter);
+    printf("%s is in range!\n", param.parameter);
+    return 1;
+  } 
+  else 
+  {
+    printf("%s is Out of range!\n", param.parameter);
     return 0;
   }
-  printf("%s is in range!\n", parameter);
-  return 1;
 }
-
 
 int batteryIsOk(float temp, float soc, float charge) 
 {
-  return (check(0,45,temp, "Temperature") && check(20,80, soc, "State of charge") && check(0,0.8,charge, "Charge rate"));
+  Parameter params[] = {
+    {0, 45, temp, "Temperature"},
+    {20, 80, soc, "State of charge"},
+    {0, 0.8, charge, "Charge rate"}
+  };
+  
+  for (int i = 0; i < 3; i++) {
+    if (!isInRange(params[i])) {
+      return 0;
+    }
+  }
+  return 1;
 }
 
 int main() 
 {
- assert(batteryIsOk(25, 70, 0.7));
- assert(!batteryIsOk(50, 85, 0));
- return 0;
+   assert(batteryIsOk(25, 70, 0.7));
+  assert(!batteryIsOk(50, 85, 0));
+  return 0;
 }
-
