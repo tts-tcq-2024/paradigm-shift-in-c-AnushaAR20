75cbf582e310763ed8294507bb4594683adc4c8d
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-05 17:03:43 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -30,9 +30,7 @@ enum ParameterType {
     CHARGE_RATE
 };
 
-void print_message(int message_index, enum ParameterType parameter_type) {
-    printf("%s\n", messages[current_language][parameter_type][message_index]);
-}
+
 
 int is_value_out_of_range(float value, float min_value, float max_value) {
     return (value < min_value || value > max_value);
