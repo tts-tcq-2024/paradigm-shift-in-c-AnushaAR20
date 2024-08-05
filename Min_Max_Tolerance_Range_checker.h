8bdf195c16093f985c90a58de3c01e3187aae0ea
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-05 17:19:17 +0530

    Update Min_Max_Tolerance_Range_checker.h

--- Min_Max_Tolerance_Range_checker.h
+++ Min_Max_Tolerance_Range_checker.h
@@ -1,1 +1,9 @@
+#ifndef MIN_MAX_TOLERANCE_RANGE_CHECKER_H_
+#def MIN_MAX_TOLERANCE_RANGE_CHECKER_H_
 
+#include "Battery.h"
+
+int is_value_out_of_range(float value, float min_value, float max_value);
+int is_value_near_lower_limit(float value, float min_value, float tolerance);
+int is_value_near_upper_limit(float value, float max_value, float tolerance);
+#endif
