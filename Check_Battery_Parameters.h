23ecc02b3c6073a0809e330249720ad20a302928
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-05 17:22:44 +0530

    Create Check_Battery_Parameters.h

---
+++ Check_Battery_Parameters.h
@@ -0,0 +1,6 @@
+#ifndef CHECK_BATTERY_PARAMETERS_H_
+#def CHECK_BATTERY_PARAMETERS_H_
+
+#include"Battery.h"
+int check_parameter(float value, float min_value, float max_value, float tolerance, enum ParameterType parameter_type);
+#endif
