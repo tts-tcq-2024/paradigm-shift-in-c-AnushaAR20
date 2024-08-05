1514f77bbd53673c16b594b03153084201d17222
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-05 17:10:25 +0530

    Update Print_message.c

--- Print_message.c
+++ Print_message.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include "Battery.h"
 
 void print_message(int message_index, enum ParameterType parameter_type) {
     printf("%s\n", messages[current_language][parameter_type][message_index]);
