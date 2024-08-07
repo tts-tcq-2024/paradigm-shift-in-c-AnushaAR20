b2955ee0c74a50c4fca8ca776a5350f9aeed7422
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-07 20:16:18 +0530

    Update Battery.h

--- Battery.h
+++ Battery.h
@@ -1,36 +1,29 @@
 #ifndef BATTERY_H
 #define BATTERY_H
 
-#include <stdio.h>
-#include <assert.h>
-#include <string.h>
-
 // Define supported languages
 #define ENGLISH 0
 #define GERMAN 1
 
-int current_language = ENGLISH;
+// Declare global variables
+extern int current_language;
+extern const char* messages[][3][3];
 
-// Message dictionaries
-const char* messages[][3][3] = {
-    // English messages
-    {
-        {"Temperature is out of range", "Warning: Temperature - Approaching low temperature limit", "Warning: Temperature - Approaching high temperature limit"},
-        {"State of Charge is out of range", "Warning: State of Charge - Approaching discharge", "Warning: State of Charge - Approaching charge peak"},
-        {"Charge rate is out of range", "Warning: Charge rate - Approaching low charge rate", "Warning: Charge rate - Approaching high charge rate"}
-    },
-    // German messages
-    {
-        {"Temperatur ist außerhalb des Bereichs", "Warnung: Temperatur - Annäherung an untere Temperaturgrenze", "Warnung: Temperatur - Annäherung an obere Temperaturgrenze"},
-        {"Ladezustand ist außerhalb des Bereichs", "Warnung: Ladezustand - Annäherung an Entladung", "Warnung: Ladezustand - Annäherung an Ladehöhepunkt"},
-        {"Laderate ist außerhalb des Bereichs", "Warnung: Laderate - Annäherung an niedrige Laderate", "Warnung: Laderate - Annäherung an hohe Laderate"}
-    }
-};
-
+// Enum for parameter types
 enum ParameterType {
     TEMPERATURE,
     SOC,
     CHARGE_RATE
 };
+
+// Function prototypes
+void set_language(int language);
+void print_message(int message_index, enum ParameterType parameter_type);
+int is_value_out_of_range(float value, float min_value, float max_value);
+int is_value_near_lower_limit(float value, float min_value, float tolerance);
+int is_value_near_upper_limit(float value, float max_value, float tolerance);
+int check_parameter(float value, float min_value, float max_value, float tolerance, enum ParameterType parameter_type);
+int battery_is_ok(float temperature, float soc, float charge_rate);
+void test_battery(float temperature, float soc, float charge_rate, int language);
 
 #endif
