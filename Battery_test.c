5b553655e7f74d4d5e06630d71cb2775bc249353
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-07 18:10:31 +0530

    Create Battery_test.c

---
+++ Battery_test.c
@@ -0,0 +1,10 @@
+#include "Battery_test.h"
+
+void test_battery(float temperature, float soc, float charge_rate, int language) {
+    set_language(language);
+    if (battery_is_ok(temperature, soc, charge_rate)) {
+        printf("Battery parameters are within acceptable range.\n");
+    } else {
+        printf("Battery parameters are out of range!\n");
+    }
+}
