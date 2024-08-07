e5bb44c277ce6d1555c64105ba9a091b8f745f98
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-07 20:21:16 +0530

    Delete Check_Battery_Parameters.h

--- Check_Battery_Parameters.h
+++
@@ -1,6 +0,0 @@
-#ifndef CHECK_BATTERY_PARAMETERS_H_
-#define CHECK_BATTERY_PARAMETERS_H_
-
-#include"Battery.h"
-int check_parameter(float value, float min_value, float max_value, float tolerance, enum ParameterType parameter_type);
-#endif
