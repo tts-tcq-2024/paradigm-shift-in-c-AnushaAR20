d3dbf2db33121bdc8cb2f2f8e7602b782bb54026
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-07 20:39:01 +0530

    Update Check_Battery_Parameters.c

--- Check_Battery_Parameters.c
+++ Check_Battery_Parameters.c
@@ -1,16 +1,32 @@
 #include<stdio.h>
 #include "Battery.h"
+#include "check_parameter.h"
 
-int check_parameter(float value, float min_value, float max_value, float tolerance, enum ParameterType parameter_type) {
+static int check_out_of_range(float value, float min_value, float max_value, enum ParameterType parameter_type) {
     if (is_value_out_of_range(value, min_value, max_value)) {
         print_message(0, parameter_type);
-        return 0;
+        return 1;
     }
+    return 0;
+}
+
+static void check_near_lower_limit(float value, float min_value, float tolerance, enum ParameterType parameter_type) {
     if (is_value_near_lower_limit(value, min_value, tolerance)) {
         print_message(1, parameter_type);
     }
+}
+
+static void check_near_upper_limit(float value, float max_value, float tolerance, enum ParameterType parameter_type) {
     if (is_value_near_upper_limit(value, max_value, tolerance)) {
         print_message(2, parameter_type);
     }
+}
+
+int check_parameter(float value, float min_value, float max_value, float tolerance, enum ParameterType parameter_type) {
+    if (check_out_of_range(value, min_value, max_value, parameter_type)) {
+        return 0;
+    }
+    check_near_lower_limit(value, min_value, tolerance, parameter_type);
+    check_near_upper_limit(value, max_value, tolerance, parameter_type);
     return 1;
 }
