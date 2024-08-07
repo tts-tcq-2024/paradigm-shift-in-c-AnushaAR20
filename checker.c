718a198aa529f801b5e4e23a1d9f881d65385d21
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-07 16:54:20 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -12,14 +12,14 @@
 int main() 
 {
     // Test in English
-    current_language = ENGLISH;
+    //current_language = ENGLISH;
     assert(battery_is_ok(25, 70, 0.7));
     assert(!battery_is_ok(50, 85, 0));
     assert(battery_is_ok(22, 76, 0.78)); // To test warning messages
     assert(battery_is_ok(3, 21, 0.05));  // To test warning messages
     
     // Test in German
-    current_language = GERMAN;
+    //current_language = GERMAN;
     assert(battery_is_ok(25, 70, 0.7));
     assert(!battery_is_ok(50, 85, 0));
     assert(battery_is_ok(22, 76, 0.78)); // To test warning messages
