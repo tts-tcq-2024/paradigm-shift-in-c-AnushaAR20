e179d7cdf8c272f06405fe2f442bf12318bb9653
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-07 20:24:16 +0530

    Update Check_battery_IsOK.c

--- Check_battery_IsOK.c
+++ Check_battery_IsOK.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
 #include "Battery.h"
-#include "Check_battery_IsOK.h"
-#include "Check_Battery_Parameters.h"
 
 int battery_is_ok(float temperature, float soc, float charge_rate) 
 {
