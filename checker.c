904968db716b5042b931e0d3a4a3b2c359e2ae84
AnushaAR20 <167959589+AnushaAR20@users.noreply.github.com>
2024-08-07 18:09:54 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -7,14 +7,6 @@
 #include "Min_Max_Tolerance_Range_checker.h"
 #include "Set_language.h"
 
-void test_battery(float temperature, float soc, float charge_rate, int language) {
-    set_language(language);
-    if (battery_is_ok(temperature, soc, charge_rate)) {
-        printf("Battery parameters are within acceptable range.\n");
-    } else {
-        printf("Battery parameters are out of range!\n");
-    }
-}
 
 int main() 
 {
